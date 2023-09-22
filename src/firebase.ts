b117d11550e44691b422691e0166ecbe54b8faa0
vahea95 <vaheazizyan12@gmail.com>
2023-09-22 21:25:28 +0400

    first commit

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA377qI92uime9V20hHMekha0TiUZkTCxY",
+  authDomain: "fir-auth-email-pass-3239f.firebaseapp.com",
+  projectId: "fir-auth-email-pass-3239f",
+  storageBucket: "fir-auth-email-pass-3239f.appspot.com",
+  messagingSenderId: "95391398711",
+  appId: "1:95391398711:web:877289a68968be385578b2",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
