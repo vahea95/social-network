1aec65561fe62bd73bd7832fd39723c4994f2f6f
vahea95 <vaheazizyan12@gmail.com>
2023-10-02 23:35:10 +0400

    Setup firebase authentication with email/password and google

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,9 +1,43 @@
+import { auth, provider } from "../firebase";
+import { signInWithPopup, signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // @ts-ignore
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      const user = userCredential.user;
+      console.log(user);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+  const handleGoogleSignIn = async () => {
+    try {
+      const userCredential = await signInWithPopup(auth, provider);
+      const user = userCredential.user;
+      console.log(user);
+    } catch (error: any) {
+      const errorMessage = error.message;
+    }
+  };
+
   return (
     <div className="w-full flex justify-center items-center h-[100vh]">
       <div className="flex flex-col w-[400px] p-4 rounded shadow-lg">
         <h1 className="mb-8 text-3xl text-center">Sign In to your account</h1>
-        <button className="px-4 py-3 mb-4 mx-3 border flex justify-center gap-2 border-slate-200 rounded-lg text-white bg-blue-500 hover:border-slate-400 hover:text-slate-100 hover:shadow transition duration-150">
+        <button
+          className="px-4 py-3 mb-4 mx-3 border flex justify-center gap-2 border-slate-200 rounded-lg text-white bg-blue-500 hover:border-slate-400 hover:text-slate-100 hover:shadow transition duration-150"
+          onClick={handleGoogleSignIn}
+        >
           <img
             className="w-6 h-6 bg-white rounded p-1"
             src="https://www.svgrepo.com/show/475656/google-color.svg"
@@ -19,14 +53,21 @@ export const Login = () => {
               type="email"
               placeholder="Email"
               className="p-2 m-2 rounded border-inherit border-2"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <label>Password</label>
             <input
               type="password"
               placeholder="Password"
               className="p-2 m-2 rounded border-inherit border-2"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-            <button className="px-4 py-3 mt-12 my-4 mx-2 border flex justify-center gap-2 bg-blue-500 border-slate-200 rounded-lg text-white hover:border-slate-400 hover:text-slate-100 hover:shadow transition duration-150">
+            <button
+              className="px-4 py-3 mt-12 my-4 mx-2 border flex justify-center gap-2 bg-blue-500 border-slate-200 rounded-lg text-white hover:border-slate-400 hover:text-slate-100 hover:shadow transition duration-150"
+              onClick={handleSignIn}
+            >
               Login
             </button>
           </div>
--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -1,12 +1,47 @@
-import { Datepicker, Input, initTE } from "tw-elements";
-initTE({ Datepicker, Input }, { allowReinits: true });
+import { useState } from "react";
+import { signInWithPopup, createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, provider } from "../firebase";
 
-export const SignUp = () => {
+export const SignUp = (): any => {
+  const [name, setName] = useState("");
+  const [surname, setSurName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // @ts-ignore
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      const user = userCredential.user;
+      console.log(user);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  const handleGoogleSignUp = async () => {
+    try {
+      const userCredential = await signInWithPopup(auth, provider);
+      const user = userCredential.user;
+      console.log(user);
+    } catch (error: any) {
+      const errorMessage = error.message;
+    }
+  };
+
   return (
     <div className="w-full flex justify-center items-center h-[100vh]">
       <div className="flex flex-col w-[400px] p-4 rounded shadow-lg">
         <h1 className="mb-8 text-3xl text-center">Create a new account</h1>
-        <button className="px-4 py-3 mb-4 mx-3 border flex justify-center gap-2 border-slate-200 rounded-lg text-white bg-blue-500 hover:border-slate-400 hover:text-slate-100 hover:shadow transition duration-150">
+        <button
+          className="px-4 py-3 mb-4 mx-3 border flex justify-center gap-2 border-slate-200 rounded-lg text-white bg-blue-500 hover:border-slate-400 hover:text-slate-100 hover:shadow transition duration-150"
+          onClick={handleGoogleSignUp}
+        >
           <img
             className="w-6 h-6 bg-white rounded p-1"
             src="https://www.svgrepo.com/show/475656/google-color.svg"
@@ -35,14 +70,30 @@ export const SignUp = () => {
                 type="email"
                 placeholder="Email"
                 className="p-2 m-2 rounded border-inherit border-2"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
               <label>Password</label>
               <input
                 type="password"
                 placeholder="Password"
                 className="p-2 m-2 rounded border-inherit border-2"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
-              <button className="px-4 py-3 mt-12 mb-2 mx-2 border flex justify-center gap-2 bg-blue-500 border-slate-200 rounded-lg text-white hover:border-slate-400 hover:text-slate-100 hover:shadow transition duration-150">
+
+              <div className="flex justify-center mt-2">
+                <p>
+                  Already have an account?
+                  <a className="text-blue-600" href="/signIn">
+                    Login
+                  </a>
+                </p>
+              </div>
+              <button
+                className="px-4 py-3 mt-12 mb-2 mx-2 border flex justify-center gap-2 bg-blue-500 border-slate-200 rounded-lg text-white hover:border-slate-400 hover:text-slate-100 hover:shadow transition duration-150"
+                onClick={handleSignUp}
+              >
                 Sign Up
               </button>
             </div>
